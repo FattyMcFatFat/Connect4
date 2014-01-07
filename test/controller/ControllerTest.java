37b36f411115e8ce0e85d2810e785537b6fb5da9
stgebhar <fatty@trash-mail.com>
2014-01-07 13:28:10 +0100

    asdf

--- test/controller/ControllerTest.java
+++ test/controller/ControllerTest.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.Globals;
 import model.Grid;
 import model.Player;
 import junit.framework.TestCase;
@@ -9,14 +10,16 @@ public class ControllerTest extends TestCase {
     private Controller controller;
     private Grid grid;
     private Player player;
+    private Globals glob;
     private static final String newLine = "\n";
 
     @Override
     protected void setUp() {
 
-        grid = new Grid(true);
+    	glob = new Globals(6, 7);
+        grid = new Grid(true, glob);
         player = new Player();
-        controller = new Controller(grid, player);
+        controller = new Controller(grid, player, glob);
     }
 
     public void testSetCellsLoading() {
